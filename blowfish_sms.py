c6d976268d94f68fe918249cfc2e73d986a40278
Stephen A. Ridley <stephen@matasano.com>
2010-09-15 13:49:58 -0400

    added a license and did some other cosmetic changes.

--- blowfish_sms.py
+++ blowfish_sms.py
@@ -1,56 +1,34 @@
 #!/usr/bin/env python
+# BLOWFISH SMS
+#
+#   An blowfish encrypted SMS client for Android Phones.
+#   
+#   August 2010
+#   stephen@sa7ori.org
+#
+# This module is open source; you can redistribute it and/or
+# modify it under the terms of the GPL or Artistic License.
+# These licenses are available at http://www.opensource.org
+#
+# This software must be used and distributed in accordance
+# with the law. The author claims no liability for its
+# misuse.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+ __author__ = "Stephen A. Ridley <stephen@sa7ori.org>"
 import struct, types
 import android
 import threading
 import pprint
 import time
 import base64
+
 class Blowfish:
-    __author__ = "Michael Gilfix <mgilfix@eecs.tufts.edu>"
-    # blowfish.py
+    # Ripped from blowfish.py
     # Copyright (C) 2002 Michael Gilfix <mgilfix@eecs.tufts.edu>
-    #
-    # This module is open source; you can redistribute it and/or
-    # modify it under the terms of the GPL or Artistic License.
-    # These licenses are available at http://www.opensource.org
-    #
-    # This software must be used and distributed in accordance
-    # with the law. The author claims no liability for its
-    # misuse.
-    #
-    # This program is distributed in the hope that it will be useful,
-    # but WITHOUT ANY WARRANTY; without even the implied warranty of
-    # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-    #
-
-    # This software was modified by Ivan Voras: CTR cipher mode of
-    # operation was added, together with testing and example code.
-    # These changes are (c) 2007./08. Ivan Voras <ivoras@gmail.com>
-    # These changes can be used, modified ad distributed under the
-    # GPL or Artistic License, the same as the original module.
-    # All disclaimers of warranty from the original module also
-    # apply to these changes.
-    """
-    Blowfish Encryption
-
-    This module is a pure python implementation of Bruce Schneier's
-    encryption scheme 'Blowfish'. Blowish is a 16-round Feistel Network
-    cipher and offers substantial speed gains over DES.
-
-    The key is a string of length anywhere between 64 and 448 bits, or
-    equivalently 8 and 56 bytes. The encryption and decryption functions operate
-    on 64-bit blocks, or 8 byte strings.
-
-    Send questions, comments, bugs my way:
-        Michael Gilfix <mgilfix@eecs.tufts.edu>
-        
-    The module has been expanded to include CTR stream encryption/decryption
-    mode, built from the primitives from the orignal module. This change
-    did not alter any of the base Blowfish code from the original author.
-
-    The author of CTR changes is:
-    Ivan Voras <ivoras@gmail.com>
-    """
     def __init__ (self, key):
         # Cipher directions
         self.ENCRYPT = 0
